21954bbb324e655ef6f089045ba11f6b217fd222
selgas <s.elgas@techsmith.com>
2024-07-07 21:52:33 -0400

    Added randomizer functions

---
+++ OpenGLGame/random.h
@@ -0,0 +1,11 @@
+#if !defined( RANDOM_H )
+#define RANDOM_H
+
+#include "common.h"
+
+void Random_Seed();
+uint32_t Random_UInt32( uint32_t min, uint32_t max );
+uint8_t Random_Percent();
+Bool_t Random_Bool();
+
+#endif
