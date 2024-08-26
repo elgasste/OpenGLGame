ca56df0327b351c828498c70f6019d1c9a893273
selgas <s.elgas@techsmith.com>
2024-08-26 10:44:37 -0400

    More generic velocity names

--- src/game_load.c
+++ src/game_load.c
@@ -52,9 +52,9 @@ Bool_t Game_LoadData( GameData_t* gameData )
    Player_Init( player );
    player->position.x = 100.0f;
    player->position.y = 0.0f;
-   player->maxRunVelocity = 300.0f;
+   player->maxVelocity.x = 300.0f;
+   player->maxVelocity.y = 800.0f;
    player->runAcceleration = 3000.0f;
-   player->maxJumpVelocity = 800.0f;
    player->gravityDeceleration = 3000.0f;
    player->jumpFrameThreshold = 80.0f;
    player->maxJumpExtensionSeconds = 0.2f;
